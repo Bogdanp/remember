fc574a63b1cb3e9c0b3c44f1def0d142e2eec494
Bogdan Popa <bogdan@defn.io>
2020-01-13 14:11:35 +0200

    cocoa: add entry list titles

--- cocoa/remember/remember/EntryList.swift
+++ cocoa/remember/remember/EntryList.swift
@@ -10,6 +10,8 @@ import Foundation
 import SwiftUI
 
 struct EntryList: View {
+    private let title = "Pending"
+
     @Binding var entries: [Entry]
     @Binding var currentEntry: Entry?
 
@@ -20,6 +22,27 @@ struct EntryList: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: 3) {
+            HStack {
+                Text(title.uppercased())
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                Spacer()
+                Text("\(entries.count)")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                    .padding(.leading, 5)
+                    .padding(.trailing, 5)
+                    .overlay(
+                        Capsule(style: .continuous)
+                            .stroke(Color.secondary, lineWidth: 1)
+                    )
+            }
+            .padding(.top, 5)
+            .padding(.bottom, 5)
+            .padding(.leading, 10)
+            .padding(.trailing, 10)
+            .opacity(0.75)
+
             ForEach(visibleEntries()) { entry in
                 EntryListItem(entry, isCurrent: self.currentEntry.map { entry.id == $0.id } ?? false)
             }
