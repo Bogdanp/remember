6716f9b791b441536ddb0a70b284ebe2cd5045ab
Bogdan Popa <bogdan@defn.io>
2020-01-28 11:26:51 +0200

    build: fix PS syntax

--- make.ps1
+++ make.ps1
@@ -1,7 +1,7 @@
-if (Test-Path -Path .\build) Remove-Item -Recurse -Path .\build
+if (Test-Path -Path .\build) { Remove-Item -Recurse -Path .\build }
 New-Item -ItemType Directory -Force -Path .\build
 
-if (Test-Path -Path .\temp)  Remove-Item -Recurse -Path .\temp
+if (Test-Path -Path .\temp) { Remove-Item -Recurse -Path .\temp }
 New-Item -ItemType Directory -Force -Path .\temp
 
 raco exe -o .\temp\remember-core.exe .\core\main.rkt
