c46ef6bf31492e325b86535715ba00c3f324cca8
Bogdan Popa <bogdan@defn.io>
2020-01-28 11:22:50 +0200

    build: add make.ps1

---
+++ make.ps1
@@ -0,0 +1,8 @@
+if (Test-Path -Path .\build) Remove-Item -Recurse -Path .\build
+New-Item -ItemType Directory -Force -Path .\build
+
+if (Test-Path -Path .\temp)  Remove-Item -Recurse -Path .\temp
+New-Item -ItemType Directory -Force -Path .\temp
+
+raco exe -o .\temp\remember-core.exe .\core\main.rkt
+raco distribute .\build .\temp\remember-core.exe
