52eec5bbed760695e2674236bd58496d93b54fcd
Bogdan Popa <bogdan@defn.io>
2020-01-28 13:08:38 +0200

    winforms: add basic f# project

---
+++ winforms/Remember/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+open System.Windows.Forms
+
+[<EntryPoint>]
+[<STAThread>]
+let main argv =
+    Application.EnableVisualStyles()
+    Application.SetCompatibleTextRenderingDefault true
+    use form = new Form()
+    Application.Run(form)
+    0
