cfaf19bc8ff3854e09a044460f7cadaca0b0d41c
Bogdan Popa <bogdan@defn.io>
2024-03-24 17:35:07 +0200

    core: drop no-longer-used compatibility-lib dep

--- core/info.rkt
+++ core/info.rkt
@@ -3,7 +3,6 @@
 (define version "2024.03.24")
 (define collection "remember")
 (define deps  '("base"
-                "compatibility-lib"
                 "db-lib"
                 "deta-lib"
                 "gregor-lib"
