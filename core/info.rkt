ea9fbcb643bcc12ae5a92f1f1579f2a5a420d448
Bogdan Popa <bogdan@defn.io>
2024-03-24 13:38:02 +0200

    build: bump version & add noise-serde-lib dep

--- core/info.rkt
+++ core/info.rkt
@@ -1,11 +1,12 @@
 #lang info
 
-(define version "2021.10.27")
+(define version "2024.03.24")
 (define collection "remember")
 (define deps  '("base"
                 "compatibility-lib"
                 "db-lib"
                 "deta-lib"
                 "gregor-lib"
+                "noise-serde-lib"
                 "threading-lib"))
 (define build-deps '("rackunit-lib"))
